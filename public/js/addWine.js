46416818ac99e902e687dce57d17d665f89c5406
Hai Arbiv <hai@hexa3d.io>
2022-11-15 23:31:01 +0200

    Validtion to add wine form

--- public/js/addWine.js
+++ public/js/addWine.js
@@ -3,87 +3,103 @@ $(() => {
     document.getElementById("addWine").onsubmit = function (evant) { myFunction(evant) };
 
     function myFunction (evant) {
-        //     var gr = [];
-        //     var arr = document.querySelectorAll(".form-check-input");
-        //     for (let i = 0; i < 10; i++) { if (arr[i].checked) { gr.push(arr[i].id) } }
-        //     alert(gr)
-
+            var gr = [];
+            var arr = document.querySelectorAll(".form-check-input");
+            for (let i = 0; i < 10; i++) { if (arr[i].checked) { gr.push(arr[i].id) } }
+           
+        var error_massege="";
         var name = document.getElementById('name').value;
-        var nameRGEX = /^[a-zA-Z]+$/;
+        var nameRGEX = /^[a-z A-Z]+$/;
         var nameResult = nameRGEX.test(name);
         if (nameResult == false) {
-            alert("name: allow letters only / Can't be empty");
+            error_massege+="name: allow letters only / Can't be empty\n";
             event.preventDefault();
             event.stopPropagation();
+          
         }
+      
 
-        if (name.length < 2) {
-            alert("name: Minimum allowed length is 2");
+        if (name.length < 2&&name.length>0) {
+            error_massege+="name: Minimum allowed length is 2\n";
             event.preventDefault();
             event.stopPropagation();
         }
         
 
         if (name.length > 20) {
-            alert("name: Maximum allowed length is 2 ");
+            error_massege+="name: Maximum allowed length is 2 \n";
             event.preventDefault();
             event.stopPropagation();
         }
 
         var country = document.getElementById('country').value;
-        var countryRGEX= require('country-regex');
+        var countryRGEX = /^[a-z A-Z]+$/;
         var countryResult = countryRGEX.test(country);
         if (countryResult == false) {
-            alert("country: allow country name only");
+            error_massege+= "country: allow letters only / Can't be empty\n";
             event.preventDefault();
             event.stopPropagation();
+          
         }
 
-        var Year = document.getElementById('Year').value;
+        var region = document.getElementById('region').value;
+        var regionRGEX = /^[a-z A-Z]+$/;
+        var regionResult = regionRGEX.test(region);
+        if (regionResult == false) {
+            error_massege+= "region: allow letters only / Can't be empty\n";
+            event.preventDefault();
+            event.stopPropagation();
+          
+        }
+
+        var winery = document.getElementById('winery').value;
+        var wineryRGEX = /^[a-z A-Z0-9]+$/;
+        var wineryResult = wineryRGEX.test(winery);
+        if (wineryResult == false) {
+            error_massege+= "winery: allow letters only / Can't be empty\n";
+            event.preventDefault();
+            event.stopPropagation();
+          
+        }
+     
+        var Year = document.getElementById('year').value;
         var YearRGEX = /^\d+$/;
         var YearResult = YearRGEX.test(Year);
         if (YearResult == false) {
-            alert("Year: allow numbers only / Can't be empty");
+            error_massege+="Year: allow numbers only / Can't be empty\n";
             event.preventDefault();
             event.stopPropagation();
         }
 
-        if (Year < 1900) {
-            alert("Year: Minimum value is 1900");
+        if (Year < 1900&&Year>0) {
+            error_massege+="Year: Minimum value is 1900\n";
             event.preventDefault();
             event.stopPropagation();
         }
 
         if (Year > 2022) {
-            alert("Year: Maximum value is: 2022");
+            error_massege+="Year: Maximum value is: 2022\n";
             event.preventDefault();
             event.stopPropagation();
         }
 
-        var type=document.getElementById('type').value; 
-        var typeRGEX = /^[a-zA-Z]+$/;
-        var typeResult=typeRGEX.test(type);
-
-        if(typeResult==false){
-            alert("type: select one option");
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        
+               
         var rate=document.getElementById('rate').value;
-        var rateRGEX = /^\d+$/;
+        var rateRGEX = /^[0-9]\d*(\.\d+)?$/;
         var rateResult=rateRGEX.test(rate);
         if (rateResult == false) {
-            alert("rate: allow numbers only / Can't be empty");
+            error_massege+="rate: allow numbers only / Can't be empty\n";
             event.preventDefault();
             event.stopPropagation();
         }
         if(rate>5 ||rate<1){
-            alert("rate: allow number is between 1-5");
+            error_massege+="rate: allow number is between 1-5\n";
             event.preventDefault();
             event.stopPropagation();
         }
 
+        if (error_massege)
+            alert(error_massege);
 
 
     }
