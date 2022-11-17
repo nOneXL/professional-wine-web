a45d217e77bff4f22f122b3a3724c100b914e6af
Gil Chen <Gtorrent12@gmail.com>
2022-11-17 15:10:04 +0200

    Implement User Model

---
+++ routes/credentials.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+    res.render("login.pug");
+});
+
+router.get("/register", (req, res) => {
+    res.render("register.pug");
+});
+
+export { router as credentialRouter };
